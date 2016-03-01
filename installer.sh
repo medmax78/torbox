772b905f0ef975df7cd051f421b22221ab2902a4
znoxx <nzabrod@yandex.ru>
2016-03-01 12:18:26 +0000

    installer.sh - added installation of tor, privoxy, i2p, initial scripts.

--- installer.sh
+++ installer.sh
@@ -4,3 +4,30 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 echo "Starting installer..."
+
+##
+echo "Installing tor..."
+./scripts/install/torinst.sh
+
+##
+echo "Installing privoxy..."
+./scripts/install/privoxyinst.sh
+
+##
+echo "Installing Java 8 for ARM..."
+./scriprs/install/java8inst.sh
+
+##
+echo "Copying needed scripts to /usr/local/bin..."
+chmod a+x ./scripts/usr/local/bin/*
+cp ./scripts/usr/local/bin/* /usr/local/bin/
+
+##
+echo "Installing i2p.."
+./scripts/install/i2pinst.sh
+
+##
+echo "Installing nescessary hardware modules..."
+. ./config.inc
+
+
