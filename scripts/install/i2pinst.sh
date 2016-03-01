d38e88df8529ee38e704392b3446181953dca794
znoxx <nzabrod@yandex.ru>
2016-03-01 18:36:09 +0000

    i2pinst.sh edited online with Bitbucket

--- scripts/install/i2pinst.sh
+++ scripts/install/i2pinst.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-. ../../config.inc
+. ./config.inc
 I2PURL=http://download.i2p2.de/releases/0.9.24/i2pinstall_0.9.24.jar
 I2PJAR=i2pinstall_0.9.24.jar
 
@@ -18,18 +18,17 @@ su -c "cp libjbigi-linux-armv6.so /opt/i2p/libjbigi.so" ${I2P_USER}
 cd ..
 rm -rf unpackedjar
 su -c "/usr/local/bin/starti2p.sh" ${I2P_USER}
-sleep 10
+sleep 20
 su -c "/usr/local/bin/stopi2p.sh" ${I2P_USER}
 su -c "sed -i 's/clientApp.0.args=7657\s*::1,127.0.0.1\s*.\/webapps\//clientApp.0.args=7657 0.0.0.0 .\/webapps\//' /home/orangepi/.i2p/clients.config" ${I2P_USER}
 su -c "sed -i 's/clientApp.4.startOnLoad=true/clientApp.4.startOnLoad=false/'  /home/orangepi/.i2p/clients.config" ${I2P_USER}
 su -c "/usr/local/bin/starti2p.sh" ${I2P_USER}
 rm -rf ./$I2PJAR
-cp ../etc/init.d/i2p /etc/init.d/i2p
+cp ./scripts/etc/init.d/i2p /etc/init.d/i2p
+sed -i "s/USER=orangepi/USER=${I2P_USER}/" /etc/init.d/i2p
 chmod a+x /etc/init.d/i2p
 systemctl enable i2p
 echo "Sleeping for 30 seconds to let i2p start/settle..."
 sleep 30
 systemctl disable i2p
 echo "I2P installation finised, hopefully without errors..."
-
-
