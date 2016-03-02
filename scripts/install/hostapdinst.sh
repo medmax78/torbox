b5c25b16ac5ec745916a50bfa3de20cd1ace73b6
orangepi <orangepi@OrangePI>
2016-03-02 14:15:50 +0300

    hostapd installer

---
+++ scripts/install/hostapdinst.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+. ./config.inc
+
+echo "Installing hostapd..."
+tar -xzvf ./hostapd/hostapd_arm.tar.gz -C /usr/local/bin
+mkdir /etc/hostapd
+cp ./scripts/etc/hostapd/hostapd.conf /etc/hostapd.conf
+echo "Applying hostapd conf..."
+sed -i "s/ssid=orangeBox/ssid=${SSID_NAME}/" /etc/hostapd/hostapd.conf
+echo "Adding to init.d..."
+cp ./scripts/etc/init.d/hostapd /etc/init.d/hostapd
+systemctl enable hostapd
+
+
