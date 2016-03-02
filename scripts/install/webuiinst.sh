cfa4e356564f4e27180fa079d3439ddee3f653c3
znoxx <nzabrod@yandex.ru>
2016-03-02 13:41:01 +0000

    webuiinst.sh edited online with Bitbucket

--- scripts/install/webuiinst.sh
+++ scripts/install/webuiinst.sh
@@ -2,8 +2,37 @@
 . ./config.inc
 ./scripts/install/nodejsinst.sh
 
-echo "Unpacking webui script to location"
+echo "Installing sudo..."
+apt-get -y install sudo
+
+echo "Adding user ${WEBUI_USER} to system and adding it to sudoers..."
+useradd ${WEBUI_USER} -s /bin/false
+
+echo " " >>/etc/sudoers
+cat ./scripts/etc/sudoers.example >>/etc/sudoers
+
+mkdir ${WEBUI_LOCATION}
+
+echo "Copying Web UI script to location..."
+
+cp -r ./webui/* ${WEBUI_LOCATION}
+
+chown -R ${WEBUI_USER}:${WEBUI_USER} ${WEBUI_LOCATION}
+
+CURRENTDIR=`pwd`
+
+cd ${WEBUI_LOCATION}
+
+su -c 'npm install' ${WEBUI_USER}
 
 echo "Enabling webui"
+
+cd $CURRENTDIR
+
+cp ./scripts/etc/init.d/webui /etc/init.d/webui
+
+systemctl enable webui
+systemctl start webui
+
 
 
