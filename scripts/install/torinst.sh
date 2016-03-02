ff479e46211fdd042bcecf715a02f567b3c9a1b7
znoxx <nzabrod@yandex.ru>
2016-03-02 14:37:58 +0000

    torinst.sh edited online with Bitbucket

--- scripts/install/torinst.sh
+++ scripts/install/torinst.sh
@@ -3,21 +3,28 @@ apt-get -y remove tor
 apt-get -y remove torsocks
 apt-get -y purge tor
 apt-get -y remove deb.torproject.org-keyring
-apt-get -y clean deb.torproject.org-keyring
+apt-get -y purge deb.torproject.org-keyring
 
-####This part is for installing from tor offical repo. 
-echo "deb http://deb.torproject.org/torproject.org jessie main" | tee /etc/apt/sources.list.d/tor-repo.list
-echo "deb-src http://deb.torproject.org/torproject.org jessie main" | tee -a /etc/apt/sources.list.d/tor-repo.list
-gpg --keyserver keys.gnupg.net --recv 886DDD89
-gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
-apt-get update
-apt-get -y install tor deb.torproject.org-keyring
+if [ ${USE_STOCK_TOR} = "1" ]; then
+   #### This part is for installing stock tor
+   echo "Installing stock tor..."
+   rm /etc/apt/sources.list.d/tor-repo.list
+   apt-get update
+   apt-get -y install tor
+   systemctl unmask tor.service   
+else
+   ####This part is for installing from tor offical repo.
+   echo "Installing tor from torproject repo..."
+   echo "deb http://deb.torproject.org/torproject.org jessie main" | tee /etc/apt/sources.list.d/tor-repo.list
+   echo "deb-src http://deb.torproject.org/torproject.org jessie main" | tee -a /etc/apt/sources.list.d/tor-repo.list
+   gpg --keyserver keys.gnupg.net --recv 886DDD89
+   gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
+   apt-get update
+   apt-get -y install tor deb.torproject.org-keyring
+fi
 
-####This part is for installing from tor debian offical repo. Comment previous part in case of any issues and uncomment next strings
-#rm /etc/apt/sources.list.d/tor-repo.list
-#apt-get update
-#apt-get -y install tor
-#systemctl unmask tor.service
+
+
 
 
 systemctl stop tor
