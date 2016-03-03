11a0688fc1bd967e9fbbb9cd4d47f1214d022e43
znoxx <nzabrod@yandex.ru>
2016-03-03 14:46:22 +0000

    hostapdinst.sh edited online with Bitbucket

--- scripts/install/hostapdinst.sh
+++ scripts/install/hostapdinst.sh
@@ -7,7 +7,7 @@ mkdir /etc/hostapd
 cp ./scripts/etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf
 echo "Applying hostapd conf..."
 sed -i "s/ssid=orangeBox/ssid=${SSID_NAME}/" /etc/hostapd/hostapd.conf
-sed -i "s/wpa_passphrase=not4youreyes/wpa_passpharase=${SSID_PASSWORD}/" /etc/hostapd/hostapd.conf
+sed -i "s/wpa_passphrase=not4youreyes/wpa_passphrase=${SSID_PASSWORD}/" /etc/hostapd/hostapd.conf
 echo "Adding to init.d..."
 cp ./scripts/etc/init.d/hostapd /etc/init.d/hostapd
 update-rc.d hostapd defaults
