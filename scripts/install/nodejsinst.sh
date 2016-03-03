9b1c6ac1084e968aca22b215ed0418654b497375
znoxx <nzabrod@yandex.ru>
2016-03-03 14:53:41 +0000

    nodejsinst.sh edited online with Bitbucket

--- scripts/install/nodejsinst.sh
+++ scripts/install/nodejsinst.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-apt-get -y install curl
+apt-get -y install curl > /dev/null
 curl --silent --location https://deb.nodesource.com/setup_5.x | sudo bash -
-apt-get -y install nodejs
+apt-get -y install nodejs > /dev/null
