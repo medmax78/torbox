be7c15d283fbcd656a5df472452001290134419d
znoxx <nzabrod@yandex.ru>
2016-03-03 11:12:09 +0000

    installer.sh edited online with Bitbucket

--- installer.sh
+++ installer.sh
@@ -13,6 +13,8 @@ echo "Starting installer..."
 echo "Copying needed scripts to /usr/local/bin..."
 chmod a+x ./scripts/usr/local/bin/*
 cp ./scripts/usr/local/bin/* /usr/local/bin/
+chmod a+x ./hardware/${HARDWARE}/usr/local/bin/*
+cp -r ./hardware/${HARDWARE}/usr/local/bin/* /usr/local/bin/*
 
 
 if [ ${HARDWARE} = "orangepipc" ]; then
@@ -61,7 +63,7 @@ echo "rtnet7601Uap" >>/etc/modules
 
 cp -r ./hardware/${HARDWARE}/${KERNEL_VERSION}/wifi/etc/* /etc/
 cp -r ./hardware/${HARDWARE}/${KERNEL_VERISON}/wifi/firmware/* /lib/firmware/
-cp -r ./hardware/${HARDWARE}/usr/local/bin/* /usr/local/bin/*
+
 echo "Disabling old 8188eu modules"
 mkdir /lib/modules-disabled
 mv /lib/modules/${KERNEL_VERISON}/kernel/drivers/net/wireless/rtl8188eu /lib/modules-disabled/
