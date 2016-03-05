13f7aa9e45824100b6f54312544727e2aca9ed03
Nikolay O. Zabrodotskiy <nzabrod@yandex.ru>
2016-03-05 14:11:49 +0300

    Last minute fixes

--- scripts/usr/local/bin/ap.sh
+++ scripts/usr/local/bin/ap.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
-wireless=`cat /sys/class/net/wlan0/operstate`
 wired=`cat /sys/class/net/eth0/operstate`
-if [ "$wired" = "up" ] && [ "$wireless" != "down" ]; then
+if [ "$wired" = "up" ]; then
   /usr/local/bin/start_ap.sh
 fi
 
