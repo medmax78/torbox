6fdaa27d7d2e5e58bac220726502e137cdfa6b00
Nikolay O. Zabrodotskiy <nzabrod@yandex.ru>
2016-03-05 14:21:16 +0300

    AP startup fix

--- scripts/usr/local/bin/ap.sh
+++ scripts/usr/local/bin/ap.sh
@@ -1,8 +1,6 @@
 #! /bin/sh
+wireless=`cat /sys/class/net/wlan0/operstate`
 wired=`cat /sys/class/net/eth0/operstate`
-if [ "$wired" = "up" ]; then
+if [ "$wireless" != "down" ] || [ "$wired" = "up" ]; then
   /usr/local/bin/start_ap.sh
 fi
-
-
-
