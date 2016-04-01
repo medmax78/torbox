14ff46928056adee462f9085ab4ea25c66a9bc49
znoxx <nzabrod@yandex.ru>
2016-04-01 17:07:10 +0000

    start_ap.sh default mode is "direct"

--- scripts/usr/local/bin/start_ap.sh
+++ scripts/usr/local/bin/start_ap.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 /usr/local/bin/masquerade.sh wlan0 eth0
-/usr/local/bin/all_tor.sh tor
+/usr/local/bin/all_tor.sh direct
 touch /var/log/ap.log
 DATE=`date`
 echo "$DATE Access point started..." >/run/ap_stamp.log
