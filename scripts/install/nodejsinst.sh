92662857b28f1ae823c8aabf1bbfd37b99a17cb2
znoxx <nzabrod@yandex.ru>
2016-05-10 13:36:49 +0000

    nodejsinst.sh edited online with Bitbucket

--- scripts/install/nodejsinst.sh
+++ scripts/install/nodejsinst.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
-apt-get -y install curl > /dev/null
-curl --silent --location https://deb.nodesource.com/setup_5.x | sudo bash -
-apt-get -y install nodejs > /dev/null
+. ./config.inc
+
+if [ ${HARDWARE} = "orangepipc" ]  || [ ${HARDWARE} = "raspberrypi2" ]; then
+  apt-get -y install curl > /dev/null
+  curl --silent --location https://deb.nodesource.com/setup_5.x | sudo bash -
+  apt-get -y install nodejs > /dev/null
+fi
+
+if [ ${HARDWARE} = "raspberrypi1" ]; then
+ wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+ sudo dpkg -i node_latest_armhf.deb
+ ln -s /usr/local/bin/node /usr/bin/node
+ ln -s /usr/local/bin/npm /usr/bin/npm
+fi
+
