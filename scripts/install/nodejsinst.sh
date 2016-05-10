e2364878db621a1d53cc637dccfb32036626e513
znoxx <nzabrod@yandex.ru>
2016-05-10 17:17:35 +0000

    nodejsinst.sh edited online with Bitbucket

--- scripts/install/nodejsinst.sh
+++ scripts/install/nodejsinst.sh
@@ -9,8 +9,9 @@ fi
 
 if [ ${HARDWARE} = "raspberrypi1" ]; then
  wget http://node-arm.herokuapp.com/node_latest_armhf.deb
- sudo dpkg -i node_latest_armhf.deb
+ dpkg -i node_latest_armhf.deb
  ln -s /usr/local/bin/node /usr/bin/node
  ln -s /usr/local/bin/npm /usr/bin/npm
+ rm -f node_latest_armhf.deb
 fi
 
