88c8578a4d0338d594091153f09a8b304147f5f4
U-ZNOBOOK\nzabr <nzabrod@yandex.ru>
2017-03-01 23:06:50 +0300

    Changed hostapd name to avoid conflicts.

--- scripts/install/hostapdinst.sh
+++ scripts/install/hostapdinst.sh
@@ -16,18 +16,18 @@ fi
 
 
 mkdir /etc/hostapd
-cp ./scripts/etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf
+cp ./scripts/etc/hostapd/hostapd-torbox.conf /etc/hostapd/hostapd-torbox.conf
 echo "Applying hostapd conf..."
-sed -i "s/ssid=orangeBox/ssid=${SSID_NAME}/" /etc/hostapd/hostapd.conf
-sed -i "s/wpa_passphrase=not4youreyes/wpa_passphrase=${SSID_PASSWORD}/" /etc/hostapd/hostapd.conf
+sed -i "s/ssid=orangeBox/ssid=${SSID_NAME}/" /etc/hostapd/hostapd-torbox.conf
+sed -i "s/wpa_passphrase=not4youreyes/wpa_passphrase=${SSID_PASSWORD}/" /etc/hostapd/hostapd-torbox.conf
 
 if [ ${HARDWARE} = "raspberrypi3" ] || [ ${HARDWARE} = "orangepi0" ]; then
-  sed -i "s/driver=rtl871xdrv/#driver=rtl871xdrv/" /etc/hostapd/hostapd.conf
-  sed -i "s/#driver=nl80211/driver=nl80211/" /etc/hostapd/hostapd.conf
+  sed -i "s/driver=rtl871xdrv/#driver=rtl871xdrv/" /etc/hostapd/hostapd-torbox.conf
+  sed -i "s/#driver=nl80211/driver=nl80211/" /etc/hostapd/hostapd-torbox.conf
 fi
 
-echo "Adding to init.d..."
-cp ./scripts/etc/init.d/hostapd /etc/init.d/hostapd
-update-rc.d hostapd defaults
-update-rc.d hostapd enable
+echo "Adding to systemd..."
+cp ./scripts/etc/systemd/system/hostapd-torbox.service /etc/systemd/system/hostapd-torbox.service
+systemctl daemon-reload
+systemctl enable hostapd-torbox
 
